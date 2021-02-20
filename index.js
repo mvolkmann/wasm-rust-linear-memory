30e68a0d439ba8bdd643e4d0437b6b7638cc011d
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2021-02-20 16:38:36 -0600

    changed to use Float64 instead of Uint8

--- index.js
+++ index.js
@@ -2,24 +2,22 @@ import wasmInit from './pkg/wasm_rust_linear_memory.js';
 
 async function run() {
   const rustWasm = await wasmInit('./pkg/wasm_rust_linear_memory_bg.wasm');
+  const bufPtr = rustWasm.get_buffer_pointer();
 
-  // Write to the WASM buffer.
-  rustWasm.store0(19);
+  // Ask Rust to store a value at index 0.
+  rustWasm.store0(19.123);
 
-  // Create a Uint8Array mapped to the WASM buffer.
-  let wasmMemory = new Uint8Array(rustWasm.memory.buffer);
+  // Create a typed array mapped to the Rust buffer.
+  let wasmMemory = new Float64Array(rustWasm.memory.buffer, bufPtr, 10);
 
-  // Get a pointer to the WASM buffer.
-  let bufPtr = rustWasm.get_buffer_pointer();
+  // Get the value at index 0 of the JS array.
+  console.log('first value =', wasmMemory[0]); // should log 19.123
 
-  // Read the value at index zero of the buffer.
-  console.log(wasmMemory[bufPtr + 0]); // should log "19"
+  // Write to index 1 of the JS array.
+  wasmMemory[1] = 7.654;
 
-  // Write to index one of the Uint8Array.
-  wasmMemory[bufPtr + 1] = 7;
-
-  // Have WASM read index one of the buffer.
-  console.log(rustWasm.read1()); // should log "7"
+  // Get the value at index 1 of the Rust buffer.
+  console.log('second value =', rustWasm.read1()); // should log 7.654
 }
 
 run();
--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,29 @@
 use wasm_bindgen::prelude::*;
 
-const BUFFER_SIZE: usize = 2;
+const BUFFER_SIZE: usize = 10; // but only using first two here
 
-static mut BUFFER: [u8; BUFFER_SIZE] = [0; BUFFER_SIZE];
+// Create array pre-populated with zero values.
+static mut BUFFER: [f64; BUFFER_SIZE] = [0.0; BUFFER_SIZE];
+
+// This just enables writing to the browser DevTools console from Rust.
+#[wasm_bindgen]
+extern "C" {
+    #[wasm_bindgen(js_namespace = console)]
+    fn log(s: &str);
+}
 
 #[wasm_bindgen]
-pub fn store0(value: u8) {
+pub fn store0(value: f64) {
+    //log(&format!("store0: value = {}", value));
     unsafe {
         BUFFER[0] = value;
+        //log(&format!("store0: buffer = {:?}", BUFFER));
     }
 }
 
 #[wasm_bindgen]
-pub fn get_buffer_pointer() -> *const u8 {
-    let pointer: *const u8;
+pub fn get_buffer_pointer() -> *const f64 {
+    let pointer: *const f64;
     unsafe {
         pointer = BUFFER.as_ptr();
     }
@@ -21,10 +31,12 @@ pub fn get_buffer_pointer() -> *const u8 {
 }
 
 #[wasm_bindgen]
-pub fn read1() -> u8 {
-    let value: u8;
+pub fn read1() -> f64 {
+    let value: f64;
     unsafe {
+        //log(&format!("read1: buffer = {:?}", BUFFER));
         value = BUFFER[1];
     }
+    //log(&format!("read1: value = {}", value));
     value
 }
